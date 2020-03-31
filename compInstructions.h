197f1939f5b2ec47983b34a24ca4172e96d6191c
Oliver Matonoha <oliver.matonoha@centrum.cz>
2020-03-31 12:40:28 +0200

    edited filename

---
+++ compInstructions.h
@@ -0,0 +1,8 @@
+#define INPUTFORMAT 0 
+
+// 1 : local aurora trees 
+
+// 2 : esd files 
+
+#include "cuts01.h" 
+
--- inputformat.h
+++
@@ -1,5 +0,0 @@
-#define INPUTFORMAT 2
-////
-// 1: local trees
-// 2: esd trees for analysisTask
-////
