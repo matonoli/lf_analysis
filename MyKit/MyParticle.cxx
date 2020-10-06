0cb9b37ecf724f4c9b76beed7ed6f3af270900de
Unknown <oliver.matonoha@centrum.cz>
2020-10-06 14:31:45 +0200

    mc y fix

--- MyKit/MyParticle.cxx
+++ MyKit/MyParticle.cxx
@@ -14,3 +14,13 @@ Int_t MyParticle::GetSign() const
     return 0;
   return TMath::Nint(ppdg->Charge());
 }
+
+#if INPUTFORMAT == 1
+Int_t MyParticle::GetY(Int_t Sp) const
+{
+	TLorentzVector pVec;
+	const Float_t MASSES[NSPECIES] = {0., 0.497614, 1.11568, 1.11568};
+	pVec.SetPtEtaPhiM(this->GetPt(),this->GetEta(),this->GetPhi(),MASSES[Sp]);
+	return pVec.Rapidity();	  
+}
+#endif
--- MyKit/MyParticle.h
+++ MyKit/MyParticle.h
@@ -36,7 +36,8 @@ class MyParticle: public TObject {
 		Float_t GetPy() 						const { return mAliParticle->GetPt()*TMath::Sin(mAliParticle->GetPhi());};
 		Float_t GetPhi() 						const { return mAliParticle->GetPhi();};
 		Float_t GetEta() 						const { return mAliParticle->GetEta();};
-		Float_t GetY()	 						const { return mAliParticle->GetY();};
+		Float_t GetY(Int_t Sp = 0);//	 						const { return mAliParticle->GetY();};
+		// overwrites the original GetY because that one doesn't work
 		Int_t GetLabel()						const { return mAliParticle->GetLabel();};
 		void SetLabel(Int_t lab)				{ return;};
 		Int_t GetPdgCode()						const { return mAliParticle->GetPdgCode();};
@@ -48,7 +49,7 @@ class MyParticle: public TObject {
 		Float_t GetPy() 						const { return mAliParticle->Py();};
 		Float_t GetPhi() 						const { return mAliParticle->Phi();};
 		Float_t GetEta() 						const { return mAliParticle->Eta();};
-		Float_t GetY()	 						const { return mAliParticle->Y();};
+		Float_t GetY(Int_t Sp = 0)	 			const { return mAliParticle->Y();};
 		Int_t GetLabel()						const { return mLabel;};// label is fMCEvent->Particle(label)
 		void SetLabel(Int_t lab)				{ mLabel = lab;};
 		Int_t GetPdgCode()						const { return mAliParticle->GetPdgCode();};
