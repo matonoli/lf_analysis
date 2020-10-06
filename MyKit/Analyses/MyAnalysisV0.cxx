4b256048b0069c0fc27bbe286cb1780878472ca5
Unknown <oliver.matonoha@centrum.cz>
2020-10-06 15:15:05 +0200

    commented cout

--- MyKit/Analyses/MyAnalysisV0.cxx
+++ MyKit/Analyses/MyAnalysisV0.cxx
@@ -456,7 +456,7 @@ Int_t MyAnalysisV0::Make(Int_t iEv) {
 
 			}
 
-			if (p.GetPdgCode() == 310) cout << "eta " << p.GetEta() << " y " << p.GetY() << endl;
+			//if (p.GetPdgCode() == 310) cout << "eta " << p.GetEta() << " y " << p.GetY() << endl;
 				
 			if (!SelectParticle(p)) continue;		// also contains Xi's !
 			
