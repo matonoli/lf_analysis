3d6f9d5b3f88e6333dde18a88062d2b1c4b601f0
Unknown <oliver.matonoha@centrum.cz>
2023-03-01 17:13:51 +0100

    split file lists to speed up running

--- submitBeastNo18.sh
+++ submitBeastNo18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 nev=1234567890
-date=230220
+date=230301
 pid=1
 
 #u can force threads by replacing aliroot with 'taskset --cpu-list ${pid} aliroot'
@@ -112,8 +112,33 @@ root->LoadMacro("MyAnalysisV0plot.cxx+");
 EOF
 
 cd ..
-
 runlist=2017_LHC17h01
+mkdir workdir$runlist
+cd workdir$runlist
+. ../prep.sh 09 1
+pid=$((pid+1))
+aliroot -b -l <<EOF &> ${runlist}.log &
+gSystem->Load("libPWGLFspectra");
+TInterpreter* root = gInterpreter;
+root->LoadMacro("TransverseSpherocity.cxx+");
+root->LoadMacro("MyAnalysis.cxx+");
+root->LoadMacro("MyHandler.cxx+");
+root->LoadMacro("MyEvent.cxx+");
+root->LoadMacro("MyTrack.cxx+");
+root->LoadMacro("MyParticle.cxx+");
+root->LoadMacro("MyV0.cxx++");
+root->LoadMacro("MyAnalysisV0.cxx++");
+root->LoadMacro("MyAnalysisV0extract.cxx+");
+root->LoadMacro("MyAnalysisV0correct.cxx+");
+root->LoadMacro("MyAnalysisV0unfold.cxx+");
+root->LoadMacro("MyAnalysisV0syst.cxx+");
+root->LoadMacro("MyAnalysisV0plot.cxx+");
+.x ../doAnalysisV0.C(${nev},"0","../lists/beast_${runlist}_pass2.list","beast_${date}_${runlist}.root","../rootOutputs/pp16kP2MC_200512_AnalysisResults_hist.root");
+.q
+EOF
+
+cd ..
+runlist=2017_LHC17h02
 mkdir workdir$runlist
 cd workdir$runlist
 . ../prep.sh 09 1
@@ -322,6 +347,33 @@ root->LoadMacro("MyAnalysisV0plot.cxx+");
 EOF
 
 cd ..
+runlist=2017_LHC17k02
+mkdir workdir$runlist
+cd workdir$runlist
+. ../prep.sh 09 1
+pid=$((pid+1))
+aliroot -b -l <<EOF &> ${runlist}.log &
+gSystem->Load("libPWGLFspectra");
+TInterpreter* root = gInterpreter;
+root->LoadMacro("TransverseSpherocity.cxx+");
+root->LoadMacro("MyAnalysis.cxx+");
+root->LoadMacro("MyHandler.cxx+");
+root->LoadMacro("MyEvent.cxx+");
+root->LoadMacro("MyTrack.cxx+");
+root->LoadMacro("MyParticle.cxx+");
+root->LoadMacro("MyV0.cxx++");
+root->LoadMacro("MyAnalysisV0.cxx++");
+root->LoadMacro("MyAnalysisV0extract.cxx+");
+root->LoadMacro("MyAnalysisV0correct.cxx+");
+root->LoadMacro("MyAnalysisV0unfold.cxx+");
+root->LoadMacro("MyAnalysisV0syst.cxx+");
+root->LoadMacro("MyAnalysisV0plot.cxx+");
+.x ../doAnalysisV0.C(${nev},"0","../lists/beast_${runlist}_pass2.list","beast_${date}_${runlist}.root","../rootOutputs/pp16kP2MC_200512_AnalysisResults_hist.root");
+.q
+EOF
+
+
+cd ..
 runlist=2017_LHC17k
 mkdir workdirMC$runlist
 cd workdirMC$runlist
@@ -348,7 +400,33 @@ root->LoadMacro("MyAnalysisV0plot.cxx+");
 EOF
 
 cd ..
-runlist=2017_LHC17l
+runlist=2017_LHC17l00
+mkdir workdir$runlist
+cd workdir$runlist
+. ../prep.sh 09 1
+pid=$((pid+1))
+aliroot -b -l <<EOF &> ${runlist}.log &
+gSystem->Load("libPWGLFspectra");
+TInterpreter* root = gInterpreter;
+root->LoadMacro("TransverseSpherocity.cxx+");
+root->LoadMacro("MyAnalysis.cxx+");
+root->LoadMacro("MyHandler.cxx+");
+root->LoadMacro("MyEvent.cxx+");
+root->LoadMacro("MyTrack.cxx+");
+root->LoadMacro("MyParticle.cxx+");
+root->LoadMacro("MyV0.cxx++");
+root->LoadMacro("MyAnalysisV0.cxx++");
+root->LoadMacro("MyAnalysisV0extract.cxx+");
+root->LoadMacro("MyAnalysisV0correct.cxx+");
+root->LoadMacro("MyAnalysisV0unfold.cxx+");
+root->LoadMacro("MyAnalysisV0syst.cxx+");
+root->LoadMacro("MyAnalysisV0plot.cxx+");
+.x ../doAnalysisV0.C(${nev},"0","../lists/beast_${runlist}_pass2.list","beast_${date}_${runlist}.root","../rootOutputs/pp16kP2MC_200512_AnalysisResults_hist.root");
+.q
+EOF
+
+cd ..
+runlist=2017_LHC17l01
 mkdir workdir$runlist
 cd workdir$runlist
 . ../prep.sh 09 1
@@ -453,6 +531,33 @@ root->LoadMacro("MyAnalysisV0plot.cxx+");
 EOF
 
 cd ..
+
+runlist=2017_LHC17m02
+mkdir workdir$runlist
+cd workdir$runlist
+. ../prep.sh 09 1
+pid=$((pid+1))
+aliroot -b -l <<EOF &> ${runlist}.log &
+gSystem->Load("libPWGLFspectra");
+TInterpreter* root = gInterpreter;
+root->LoadMacro("TransverseSpherocity.cxx+");
+root->LoadMacro("MyAnalysis.cxx+");
+root->LoadMacro("MyHandler.cxx+");
+root->LoadMacro("MyEvent.cxx+");
+root->LoadMacro("MyTrack.cxx+");
+root->LoadMacro("MyParticle.cxx+");
+root->LoadMacro("MyV0.cxx++");
+root->LoadMacro("MyAnalysisV0.cxx++");
+root->LoadMacro("MyAnalysisV0extract.cxx+");
+root->LoadMacro("MyAnalysisV0correct.cxx+");
+root->LoadMacro("MyAnalysisV0unfold.cxx+");
+root->LoadMacro("MyAnalysisV0syst.cxx+");
+root->LoadMacro("MyAnalysisV0plot.cxx+");
+.x ../doAnalysisV0.C(${nev},"0","../lists/beast_${runlist}_pass2.list","beast_${date}_${runlist}.root","../rootOutputs/pp16kP2MC_200512_AnalysisResults_hist.root");
+.q
+EOF
+
+cd ..
 runlist=2017_LHC17m
 mkdir workdirMC$runlist
 cd workdirMC$runlist
@@ -507,6 +612,32 @@ EOF
 cd ..
 
 runlist=2017_LHC17o01
+mkdir workdir$runlist
+cd workdir$runlist
+. ../prep.sh 09 1
+pid=$((pid+1))
+aliroot -b -l <<EOF &> ${runlist}.log &
+gSystem->Load("libPWGLFspectra");
+TInterpreter* root = gInterpreter;
+root->LoadMacro("TransverseSpherocity.cxx+");
+root->LoadMacro("MyAnalysis.cxx+");
+root->LoadMacro("MyHandler.cxx+");
+root->LoadMacro("MyEvent.cxx+");
+root->LoadMacro("MyTrack.cxx+");
+root->LoadMacro("MyParticle.cxx+");
+root->LoadMacro("MyV0.cxx++");
+root->LoadMacro("MyAnalysisV0.cxx++");
+root->LoadMacro("MyAnalysisV0extract.cxx+");
+root->LoadMacro("MyAnalysisV0correct.cxx+");
+root->LoadMacro("MyAnalysisV0unfold.cxx+");
+root->LoadMacro("MyAnalysisV0syst.cxx+");
+root->LoadMacro("MyAnalysisV0plot.cxx+");
+.x ../doAnalysisV0.C(${nev},"0","../lists/beast_${runlist}_pass2.list","beast_${date}_${runlist}.root","../rootOutputs/pp16kP2MC_200512_AnalysisResults_hist.root");
+.q
+EOF
+
+cd ..
+runlist=2017_LHC17o02
 mkdir workdir$runlist
 cd workdir$runlist
 . ../prep.sh 09 1
